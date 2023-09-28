28f7b436e110bf85d9ec6a36ae33cae740bd426a
fedot Mosorkin <fmosorkin@gmail.com>
2023-09-28 20:51:56 +0900

    Revert "revert to lab1"
    This reverts commit c823601593915770e238dbb3e7d93175176e9d93.

--- mosorkinFedot/App.js
+++ mosorkinFedot/App.js
@@ -1,106 +1,52 @@
-import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import { useState } from "react";
+import { StyleSheet, View, Image } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+
+import Lab1 from "./screens/lab1.js";
+import Lab2 from "./screens/lab2.js";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
-  const [imageSize, setImageSize] = useState(100);
-  const [backgroundColor, setBackgroundColor] = useState("#FFFFFF");
-
-  const handleColorButtonClick = () => {
-    setBackgroundColor(generateRandomColor());
-  };
-
-  const increaseImageSize = () => {
-    const newSize = imageSize + 50;
-
-    if (newSize <= 350) {
-      setImageSize(imageSize + 50);
-    }
-  };
-  const decreaseImageSize = () => {
-    const newSize = imageSize - 50;
-
-    if (newSize >= 50) {
-      setImageSize(imageSize - 50);
-    }
-  };
-
-  const generateRandomColor = () => {
-    const red = Math.floor(Math.random() * 256);
-    const green = Math.floor(Math.random() * 256);
-    const blue = Math.floor(Math.random() * 256);
-    return `rgb(${red}, ${green}, ${blue})`;
-  };
-
   return (
-    <View style={styles.container(backgroundColor)}>
-      <Image
-        source={require("./imgs/logo1.png")}
-        style={{ width: imageSize, height: imageSize }}
-      />
-      <View style={styles.buttonsContainer}>
-        <TouchableOpacity
-          style={styles.sizeButtons}
-          onPress={decreaseImageSize}
-        >
-          <Text style={styles.textButton}>-</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.sizeButtons}
-          onPress={increaseImageSize}
-        >
-          <Text style={styles.textButton}>+</Text>
-        </TouchableOpacity>
-      </View>
-      <TouchableOpacity
-        style={styles.changeColorButton}
-        onPress={handleColorButtonClick}
-      >
-        <Image
-          style={styles.changeColorImg}
-          source={require("./imgs/chngcolor.png")}
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen
+          name="Lab1"
+          component={Lab1}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <View>
+                <Image
+                  source={require("./imgs/lab1icon.png")}
+                  style={styles.img}
+                />
+              </View>
+            ),
+          }}
         />
-      </TouchableOpacity>
-    </View>
+        <Tab.Screen
+          name="Lab2"
+          component={Lab2}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <View>
+                <Image
+                  source={require("./imgs/lab2icon.png")}
+                  style={styles.img}
+                />
+              </View>
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  container: (backgroundColor) => ({
-    flex: 1,
-    backgroundColor: backgroundColor,
-    alignItems: "center",
-    justifyContent: "center",
-  }),
-  sizeButtons: {
-    height: 40,
-    width: 100,
-    alignItems: "center",
-    justifyContent: "center",
-    borderRadius: 14,
-    backgroundColor: "orange",
-    marginHorizontal: 10,
-  },
-  buttonsContainer: {
-    flexDirection: "row",
-    marginTop: 300,
-    position: "absolute",
-    bottom: 80,
-  },
-  changeColorButton: {
-    width: 50,
-    height: 50,
-    right: 30,
-    position: "absolute",
-    top: 80,
-    borderWidth: 2,
-    borderColor: "black",
-    borderRadius: 10,
-  },
-  changeColorImg: {
-    width: 50,
-    height: 50,
-  },
-  textButton: {
-    fontSize: 32,
+  img: {
+    height: 30,
+    width: 30,
   },
 });
